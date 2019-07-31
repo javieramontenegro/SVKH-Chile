6e02e374b10c86a58dc6cb7e7b6890c171bc5d14
alelulu <ale.silvasuazo@gmail.com>
2019-07-31 17:15:15 -0400

    adding contrast function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import { initRouter } from "./routes.js";
+import { changeContrast } from "./assets/logic/logiccontrast.js"
+//import { biggerSize } from "./assets/logic/logicsize.js"
 
 const init = () => {
     initRouter()
 }
 
 window.addEventListener('load', init)
+
+let contrast = document.getElementById("contrast");
+contrast.addEventListener('click', () => {
+    changeContrast()
+})
+
+/* let size = document.getElementById("size");
+size.addEventListener('click', () => {
+    biggerSize();
+}) */
---
+++ src/assets/logic/logicsize.js
@@ -0,0 +1,11 @@
+/* export const biggerSize = () => {
+  let normalSize = document.getElementById("body");
+  if(normalSize.getAttribute("class") === "normal-size"){
+    normalSize.classList.remove("normal-size");
+    normalSize.classList.add("bigger-size");
+  }
+  else {
+    normalSize.classList.remove("bigger-size");
+    normalSize.classList.add("normal-size");
+  }
+} */
