d770b5e0851c892ee8a69197fb3caf267b147bad
javieramontenegro <doki.snsd@gmail.com>
2019-08-08 18:25:24 -0400

    home

--- src/app.js
+++ src/app.js
@@ -4,10 +4,53 @@ import { modifySize, backToNormal } from "./assets/logic/logicsize.js"
 
 const init = () => {
     initRouter()
+    
 }
+// FUNCION PARA BOTONES ACCESSIBILITY
+/* function myFunction() {
+    if (window.matchMedia("(max-width: 320px)").matches) { // If media query matches
+      document.getElementById("accessibility").style.left = "7rem";
+     } 
+     else if (window.matchMedia("(max-width: 375px)").matches) { // If media query matches
+        document.getElementById("accessibility").style.left = "8rem";
+       } 
+     else if (window.matchMedia("(max-width: 425x)").matches) { // If media query matches
+        document.getElementById("accessibility").style.left = "11rem";
+       } 
+     else if (window.matchMedia("(max-width: 770px)").matches) { // If media query matches
+        document.getElementById("accessibility").style.left = "30rem";
+       } 
+       else if (window.matchMedia("(max-width: 1024px)").matches) { // If media query matches
+        document.getElementById("accessibility").style.left = "40rem";
+       } 
+     
+       else if (window.matchMedia("(max-width: 2000px)").matches) { // If media query matches
+        document.getElementById("accessibility").style.left = "60rem";
+       } 
+  } */
+ /*  function screenTablets(tablets) {
+    if (tablets.matches) { // If media query matches
+      document.getElementById("accessibility").style.left = "40rem";
+     
+     } else {
+     document.getElementById("accessibility").style.left = "60rem";
+    
+   }
+  } */
+  
+  
+ // Call listener function at run time
+ /*  screenTablets(tablets) */
+ 
+
+
+
+
+
+
 
 window.addEventListener('load', init)
-
+ 
 let contrast = document.getElementById("contrast");
 contrast.addEventListener('click', () => {
     changeContrast()
--- src/assets/views/viewhome.js
+++ src/assets/views/viewhome.js
@@ -1,57 +1,56 @@
-import { viewAbout } from './viewabout.js';
-import { viewSpecialists} from './viewspecialists.js';
-import { viewSymtoms } from './viewsymtoms.js';
-import { viewTreatment } from './viewtreatment.js';
+import { viewContact } from "./viewcontact.js";
+import { viewAbout } from "./viewabout.js";
+import { viewSpecialists } from "./viewspecialists.js"
+import { viewInfo } from "./viewinfo.js"
+import { viewSymtoms } from "./viewsymtoms.js"
 
 export const viewHome = () => {
 	document.getElementById('root').innerHTML = `
 
 
-  <div class="row">
-     <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+ 
         
        <div class="row">
          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
              <img src="./assets/img/home_1.png" class="home_1"alt="home_1" >
          </div>
-         <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
-            <p class="txt-def">Somos Síndrome-Vogt-Koyanagi Harada Chile.
-             Nuestra agrupación iniciada el 2017, busca hacer visible el síndrome en Chile y el mundo.
-              Hoy estamos en proceso de convertirnos en fundación, para eso necesitamos llegar a más personas que nos reconozcan.
+         <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 align-self-center">
+            <p class="txt-def">Somos Sindrome-Vogt-Koyanagi Harada Chile.
+             Nuestra agrupación iniciada el 2017, busca hacer visible el sindrome en Chile y el mundo.
+              Hoy estamos en proceso de convertirnos en fundación, para eso nocesitamos llegar a mas personas que nos reconozcan.
         <br>
-            Conoce un poco más de SVKH Chile.
+            Conoce un poco mas de SVKH Chile.
              <p/>
             <button id="btn-to-about" class="btn blue-btn">Ver más</button>
         </div>
          
      
       </div>
-    </div>
-  </div>
+   
   <br>
            <div class="row">
-                  <h1 class="normal-color">Infórmate sobre el VKH </h1>
+                  <h1>Informate sobre el VKH </h1>
            </div>
         <br>
           
                   <div class="row">
                     <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 colInfo">
-                       <img src="./assets/img/home_3.png" class="home_2"alt="home_2" >
-                        <h3 class="normal-color"> Síntomas</h3>
-                         <p>Conoce los principales síntomas del Síndrome Vogt Koyanagi Harada. </p>
+                       <img src="./assets/img/home_2.png" class="home_2"alt="home_2" >
+                        <h3> Sintomas</h3>
+                         <p>Conoce los principales síntomas del Sindrome Vogt Koyanagi Harada. </p>
                          <button id="btn-to-symtoms" class="btn blue-btn">Ver más</button>
                     </div> 
                        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 colInfo">
-                          <img src="./assets/img/home_2.png" class="home_3"alt="ojo" >
-                           <h3 class="normal-color">Médicos en Chile</h3>
+                          <img src="./assets/img/home_3.png" class="home_3"alt="ojo" >
+                           <h3>Medicos en Chile</h3>
                             <p>Lista de médicos tratantes en Chile. </p>
                             <button id="btn-to-especialist" class="btn blue-btn">Ver más</button>
                        </div>   
                 
                        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 colInfo">
                             <img src="./assets/img/home_4.png" class="home_4"alt="tratamiento" >
-                             <h3 class="normal-color">Tratamientos</h3>
-                             <p>Guía para conocer los pricipales tratamientos, guiados por médicos especialistas. </p>
+                             <h3>Tratamientos</h3>
+                             <p>Guia para conocer los pricipales tratamientos, guiado por médicos especialistas. </p>
                              <button id="btn-to-tratment" class="btn blue-btn">Ver más</button>
                        </div>     
                   </div>  
@@ -60,22 +59,22 @@ export const viewHome = () => {
             <br>
             
                   <div class="row">
-                       <h3 class="normal-color">Agrupaciones Amigas</h3>
+                       <h3>Agrupaciones Amigas </h3>
                   </div>
                   <br>
                <div class="row">
                  
                      
                         <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-                           <img src="./assets/img/home_5.png" class="home_5"alt="Uveítis Argentina" >
+                           <img src="./assets/img/home_5.png" class="home_5"alt="uveitis Argentina" >
                             <div class="txt-center">
-                             <p class="friend">Grupo Argentina de Uveítis</p>
+                             <p class="friend">    Grupo Argentina de Uveitis </p>
                              </div>
                         </div> 
                            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-                              <img src="./assets/img/home_6.png" class="home_6"alt="México ayuda svkh" >
+                              <img src="./assets/img/home_6.png" class="home_6"alt="mexico ayuda svkh" >
                                
-                                <p class="friend">México ayuda Síndrome Vogt-Koyanagi Harada</p>
+                                <p class="friend">Mexico ayuda Sindrome Vogt-Koyanagi Harada </p>
                                 
                            </div>   
                     
@@ -107,7 +106,7 @@ document.getElementById('btn-to-especialist').addEventListener('click', () => {
   window.location.hash = '#/especialistas';
 })
 document.getElementById('btn-to-tratment').addEventListener('click', () => {
-  viewTreatment();
+  viewSpecialists();
   window.location.hash = '#/tratamientos';
 })
 }
